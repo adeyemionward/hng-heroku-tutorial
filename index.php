ee0f2a8f9f2d63a022878b63e332e3048aa3eb80
adeyemionward <adeyemiadeshina6@gmail.com>
2019-09-20 09:04:04 +0100

    index modified

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link href="starter-template.css" rel="stylesheet">
   </head>
   <body>
+  <h1>This is my new change</h1>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
